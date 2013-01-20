eb6ca90904c7b298151b0f64bbb6a7ea2a53b650
odiaseo <peleodiase@yahoo.co.uk>
2013-01-20 23:10:08 +0000

    Moved service configuration from config to module file

--- config/module.config.php
+++ config/module.config.php
@@ -2,19 +2,11 @@
 // From within a configuration file
 return array(
     'view_manager' => array(
-        'helper_map' => array(
-            'displayGrid' => 'SynergyDataGrid\View\Helper\DisplayGrid',
-        ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
         ),
     ),
 
-    'service_manager' => array(
-        'factories' => array(
-            'jqgrid' => 'SynergyDataGrid\Grid\JqGridFactory',
-        ),
-    ),
     'jqgrid' => array(
         'excluded_columns' => array(
 
