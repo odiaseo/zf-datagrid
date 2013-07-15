538969e812719980141c2d022cb545f2e5962f61
odiaseo <peleodiase@yahoo.co.uk>
2013-07-15 17:25:45 +0100

    Added option to add subgrid or subgrid_as_grid

---
+++ src/SynergyDataGrid/Grid/SubGrid.php
@@ -0,0 +1,11 @@
+<?php
+    namespace SynergyDataGrid\Grid;
+
+    class SubGrid extends Base
+    {
+        public $name = array();
+        public $width = array();
+        public $align = array();
+        public $params = array();
+
+    }
