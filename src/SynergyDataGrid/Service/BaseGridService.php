d4bb1e465dc682749dc5ed878b3cf68cd328747c
odiaseo <peleodiase@yahoo.co.uk>
2014-12-21 10:43:00 +0000

    combine filters and custom filters when searching for records on the grid

--- src/SynergyDataGrid/Service/BaseGridService.php
+++ src/SynergyDataGrid/Service/BaseGridService.php
@@ -17,128 +17,130 @@ namespace SynergyDataGrid\Service;
 use SynergyCommon\Service\BaseService;
 use SynergyDataGrid\Model\Config\ModelOptions;
 use Zend\Json\Json;
-use Zend\ServiceManager\ServiceManager;
 
 class BaseGridService
-    extends BaseService
-{
-    protected $_expression
-        = array(
-            'eq' => 'EQUAL',
-            'ne' => 'NOT_EQUAL',
-            'lt' => 'LESS_THAN',
-            'le' => 'LESS_THAN_OR_EQUAL',
-            'gt' => 'GREATER_THAN',
-            'ge' => 'GREATER_THAN_OR_EQUAL',
-            'bw' => 'BEGIN_WITH',
-            'bn' => 'NOT_BEGIN_WITH',
-            'in' => 'IN',
-            'ni' => 'NOT_IN',
-            'ew' => 'END_WITH',
-            'en' => 'NOT_END_WITH',
-            'cn' => 'CONTAIN',
-            'nc' => 'NOT_CONTAIN'
-        );
+	extends BaseService {
+	protected $_expression
+		= array(
+			'eq' => 'EQUAL',
+			'ne' => 'NOT_EQUAL',
+			'lt' => 'LESS_THAN',
+			'le' => 'LESS_THAN_OR_EQUAL',
+			'gt' => 'GREATER_THAN',
+			'ge' => 'GREATER_THAN_OR_EQUAL',
+			'bw' => 'BEGIN_WITH',
+			'bn' => 'NOT_BEGIN_WITH',
+			'in' => 'IN',
+			'ni' => 'NOT_IN',
+			'ew' => 'END_WITH',
+			'en' => 'NOT_END_WITH',
+			'cn' => 'CONTAIN',
+			'nc' => 'NOT_CONTAIN'
+		);
 
-    protected function _processFilter($data)
-    {
+	protected function _processFilter( $data ) {
 
-        $filters   = array();
-        $operation = isset($data['searchOper']) ? $data['searchOper'] : 'eq';
+		$filters   = array();
+		$operation = isset( $data['searchOper'] ) ? $data['searchOper'] : 'eq';
 
-        if (isset($data['customFilters'])) {
-            return $this->processSearchFilters($data['customFilters']);
+		if ( isset( $data['customFilters'] ) ) {
+			$filters = $this->processSearchFilters( $data['customFilters'], $filters );
 
-        } elseif (isset($data['filters'])) {
-            return $this->processSearchFilters($data['filters']);
-        } elseif (isset($data['searchField'])) {
+		}
+		if ( isset( $data['filters'] ) ) {
+			$filters = $this->processSearchFilters( $data['filters'], $filters );
+		}
 
-            // Single field filtering
-            return array(
-                'field'      => $data['searchField'],
-                'value'      => trim($data['searchString']),
-                'expression' => $this->_expression[$operation],
-                'options'    => array()
-            );
-        }
+		if ( isset( $data['searchField'] ) ) {
+			// Single field filtering
+			$filters = array(
+				'field'      => $data['searchField'],
+				'value'      => trim( $data['searchString'] ),
+				'expression' => $this->_expression[ $operation ],
+				'options'    => array()
+			);
+		}
 
-        return $filters;
-    }
+		return $filters;
+	}
 
-    /**
-     * @param $params
-     *
-     * @return mixed
-     */
-    private function  processSearchFilters($params)
-    {
-        $filter = Json::decode($params, Json::TYPE_ARRAY);
+	/**
+	 * @param       $params
+	 * @param array $combined
+	 *
+	 * @return array
+	 */
+	private function  processSearchFilters( $params, $combined = array() ) {
+		$filter = Json::decode( $params, Json::TYPE_ARRAY );
 
-        if (isset($filter['rules']) and count($filter['rules']) > 0) {
+		if ( isset( $filter['rules'] ) and count( $filter['rules'] ) > 0 ) {
+			if ( empty( $combined['rules'] ) ) {
+				$combined['rules'] = array();
+			}
+			foreach ( $filter['rules'] as $rule ) {
+				$combined['field'][]      = $rule['field'];
+				$combined['value'][]      = $rule['data'];
+				$combined['expression'][] = $this->_expression[ $rule['op'] ];
+				$combined['rules'][]      = $rule;
+			}
 
-            foreach ($filter['rules'] as $rule) {
-                $filters['field'][]      = $rule['field'];
-                $filters['value'][]      = $rule['data'];
-                $filters['expression'][] = $this->_expression[$rule['op']];
-            }
+			$combined['options']['multiple'] = true;
+			$combined['options']['boolean']  = ( isset( $filter['groupOp'] ) ) ? $filter['groupOp'] : 'AND';
 
-            $filters['options']['multiple'] = true;
-            $filters['options']['boolean']  = (isset($filter['groupOp'])) ? $filter['groupOp'] : 'AND';
+			return $combined;
+		}
 
-            return $filters;
-        }
-        return array();
-    }
+		return $combined;
+	}
 
-    /**
-     * Get domain model
-     *
-     * @param       $data
-     * @param       $className
-     * @param array $gridOptions
-     *
-     * @return \SynergyDataGrid\Model\BaseModel
-     */
-    public function getModel($className, $data = array(), $gridOptions = array())
-    {
-        $sort = array();
-        if (!empty($data['sidx'])) {
-            $sort[] = array(
-                'sidx' => $data['sidx'],
-                'sord' => isset($data['sord']) ? $data['sord'] : 'asc'
-            );
-        } else {
-            $sort = array();
-        }
+	/**
+	 * Get domain model
+	 *
+	 * @param       $data
+	 * @param       $className
+	 * @param array $gridOptions
+	 *
+	 * @return \SynergyDataGrid\Model\BaseModel
+	 */
+	public function getModel( $className, $data = array(), $gridOptions = array() ) {
+		$sort = array();
+		if ( ! empty( $data['sidx'] ) ) {
+			$sort[] = array(
+				'sidx' => $data['sidx'],
+				'sord' => isset( $data['sord'] ) ? $data['sord'] : 'asc'
+			);
+		} else {
+			$sort = array();
+		}
 
-        if (isset($data['filters']) || isset($data['customFilters'])) {
-            $filters = $this->_processFilter($data);
-        } else {
-            $filters = array();
-        }
-        $options = array(
-            'gridConfig' => $gridOptions,
-            'filters'    => $filters,
-            'grid'       => isset($data['grid']) ? $data['grid'] : null,
-            'entity'     => isset($data['entity']) ? $data['entity'] : null,
-            'page'       => isset($data['page']) ? $data['page'] : null,
-            'rows'       => isset($data['rows']) ? $data['rows'] : null,
-            'sord'       => isset($data['sord']) ? $data['sord'] : null,
-            'sidx'       => isset($data['sidx']) ? $data['sidx'] : null,
-            'presets'    => isset($data['presets']) ? $data['presets'] : array(),
-            'sortOrder'  => $sort,
-        );
+		if ( isset( $data['filters'] ) || isset( $data['customFilters'] ) ) {
+			$filters = $this->_processFilter( $data );
+		} else {
+			$filters = array();
+		}
+		$options = array(
+			'gridConfig' => $gridOptions,
+			'filters'    => $filters,
+			'grid'       => isset( $data['grid'] ) ? $data['grid'] : null,
+			'entity'     => isset( $data['entity'] ) ? $data['entity'] : null,
+			'page'       => isset( $data['page'] ) ? $data['page'] : null,
+			'rows'       => isset( $data['rows'] ) ? $data['rows'] : null,
+			'sord'       => isset( $data['sord'] ) ? $data['sord'] : null,
+			'sidx'       => isset( $data['sidx'] ) ? $data['sidx'] : null,
+			'presets'    => isset( $data['presets'] ) ? $data['presets'] : array(),
+			'sortOrder'  => $sort,
+		);
 
-        /** @var $model \SynergyDataGrid\Model\BaseModel */
-        $model = $this->_serviceManager->get('synergydatagrid\model');
-        $model->setEntityClass($className);
-        $modelOptions = new ModelOptions($options);
-        $model->setOptions($modelOptions);
+		/** @var $model \SynergyDataGrid\Model\BaseModel */
+		$model = $this->_serviceManager->get( 'synergydatagrid\model' );
+		$model->setEntityClass( $className );
+		$modelOptions = new ModelOptions( $options );
+		$model->setOptions( $modelOptions );
 
-        /** @var $entityManager \Doctrine\Orm\EntityManager */
-        $entityManager = $this->_serviceManager->get($model->getOrmKey());
-        $model->setEntityManager($entityManager);
+		/** @var $entityManager \Doctrine\Orm\EntityManager */
+		$entityManager = $this->_serviceManager->get( $model->getOrmKey() );
+		$model->setEntityManager( $entityManager );
 
-        return $model;
-    }
+		return $model;
+	}
 }
